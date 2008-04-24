bed6905f00296d08d7f3cd43cef7fec983ae4308
daniel.kummer <daniel.kummer@fe886383-234b-0410-a1ab-e127868e2f45>
2008-04-24 20:24:29 +0000

    New Planets

--- trunk/OpenGL_SceneGraph_Stussman/Code/Aufgabe4/src/planetdefs.cpp
+++ trunk/OpenGL_SceneGraph_Stussman/Code/Aufgabe4/src/planetdefs.cpp
@@ -1,14 +1,14 @@
 #include "planetdefs.h"
 
 //HACK: remove this HACK!! -> change how PlanetDef works
-#ifndef WIN32
+/*#ifndef WIN32
 #define INFINITE 0xFFFFFFFF
-#endif
+#endif*/
 
 
 
 PlanetDef::PlanetDef(float aDiameter,       // in earths
-               float aRotationPeriod, // in earthdays
+               float aRotVelocity, // in earthdays
                float aTilt,           // in degrees
                float aSemiMajorAxis,  // in AU
                float aOrbitPeriod,    // in earthyears
@@ -16,7 +16,8 @@ PlanetDef::PlanetDef(float aDiameter,       // in earths
                char* aTextureName,
                bool aAntiRot){
   radius = aDiameter/2.0f * 0.50f;
-  rotVelocity = 360.0f/aRotationPeriod;
+  //rotVelocity = 360.0f/aRotationPeriod;
+  rotVelocity = aRotVelocity;
   tilt = aTilt;
   orbitRadius = aSemiMajorAxis * 10.0f;
   orbitInclination = aOrbitInclination;
@@ -31,11 +32,41 @@ PlanetDef::~PlanetDef(){
 }
 
 
-MoonDef::MoonDef():PlanetDef(0.546f, (float)INFINITE, 1.54f, 0.257f, 0.0747f, 5.145f, "Textures/Moon.bmp", false){}
-EarthDef::EarthDef():PlanetDef(1.0f, 1.0f, 23.45f, 1.0f, 1.0f, 0.0f, "Textures/Earth.bmp"){
-  mMoons.push_back(new MoonDef());
-}
-MarsDef::MarsDef():PlanetDef(0.532f, 1.03f, 23.98f, 1.52f, 1.88f, 0.0f, "Textures/Mars.bmp"){}
+//Moons
+LunaDef::LunaDef():PlanetDef(0.546f, 0.0f, 1.54f, 0.257f, 0.0747f, 5.145f, "Textures/Moon.bmp", false){}
+TitanDef::TitanDef():PlanetDef(0.404f, 0.0f, 0.34854f, 0.832f, 211.03f, 0.0f, "Textures/Titan.bmp", false){}
+//Planets
+MercuryDef::MercuryDef():PlanetDef(0.382f, 6.13f, 0.0f, 0.39f, 0.24f, 0.0f, "Textures/Mercury.bmp"){}
+VenusDef::VenusDef():PlanetDef(0.949f, -1.48f, 177.4f, 0.72f, 0.62f, 0.0f, "Textures/Venus.bmp"){}
+EarthDef::EarthDef():PlanetDef(1.0f, 360.0f, 23.45f, 1.0f, 1.0f, 0.0f, "Textures/Earth.bmp"){mMoons.push_back(new LunaDef());}
+MarsDef::MarsDef():PlanetDef(0.532f, 349.51f, 23.98f, 1.52f, 1.88f, 0.0f, "Textures/Mars.bmp"){}
+JupiterDef::JupiterDef():PlanetDef(11.209f, 878.04f, 3.08f, 5.20f, 11.86f, 0.0f, "Textures/Jupiter.bmp"){}
+SaturnDef::SaturnDef():PlanetDef(9.44f, 818.18f, 26.73f, 9.45f, 29.46f, 0.0f, "Textures/Saturn.bmp"){mMoons.push_back(new TitanDef());}
+UranusDef::UranusDef():PlanetDef(4.007f, -500.0f, 97.92f, 19.18f, 84.01f, 0.0f, "Textures/Uranus.bmp"){}
+NeptuneDef::NeptuneDef():PlanetDef(3.883f, 500.0f, 28.8f, 30.06f, 164.8f, 0.0f, "Textures/Neptune.bmp"){}
+PlutoDef::PlutoDef():PlanetDef(0.180f, -56.42f, 122.0f, 39.44, 247.7f, 0.0f, "Texturs/Pluto.bmp"){}
+
+
+
+
+
+// form here: http://www.windows.ucar.edu/tour/link=/our_solar_system/planets_table.html
+
+//                            Mercury Venus   Earth   Mars    Jupiter Saturn  Uranus  Neptune Pluto  Moon
+//diameter(Earth=1)           0.382   0.949   1       0.532   11.209  9.44    4.007   3.883   0.180  0.546
+//rotationperiod(inEarthdays) 58.65   -243*   1       1.03    0.41    0.44    -0.72*  0.72    -6.38* 0
+//inclinationofaxis(degrees)  0.0     177.4   23.45   23.98   3.08    26.73   97.92   28.8    122    1.54
+
+//meandistancefromSun(AU)     0.39    0.72    1       1.52    5.20    9.54    19.18   30.06   39.44  0.00257
+//orbitalperiod(Earthyears)   0.24    0.62    1       1.88    11.86   29.46   84.01   164.8   247.7  27.32
+//orbitinclination(to sun)            0                                                              5.14
+
+
+//orbitaleccentricity         0.2056  0.0068  0.0167  0.0934  0.0483  0.0560  0.0461  0.0097  0.2482 
+//meanorbitalvelocity(km/sec) 47.89   35.03   29.79   24.13   13.06   9.64    6.81    5.43    4.74
+//numberofmoons               0       0       1       2       63      59      27      13      3
+//rings?                      no      no      no      no      yes     yes     yes     yes     no
+
 
 
 
