ad6d088b3e837c2c8710fb722df1f7c84765108c
david.gunzinger@gmail.com <david.gunzinger@gmail.com@fe886383-234b-0410-a1ab-e127868e2f45>
2008-04-25 09:25:36 +0000

    ring now works

--- trunk/OpenGL_SceneGraph_Stussman/Code/Aufgabe4/src/scene.cpp
+++ trunk/OpenGL_SceneGraph_Stussman/Code/Aufgabe4/src/scene.cpp
@@ -205,6 +205,15 @@ void SolarSytemScene::createScene(){
 }
 //-------------------------------------------------------//
 AbstractNode* SolarSytemScene::createPlanet(PlanetDef* aPlanetDef){
+  try
+  {
+  	RingDef* ring = dynamic_cast <RingDef*> (aPlanetDef);
+  	if(ring) 
+  		return createPlanet(ring);
+  }catch (std::bad_cast)
+  { 
+  //dynamic_cast<> failed
+  }
 
   Builder vBuilder(new TransformSeparator());
 
@@ -258,6 +267,7 @@ AbstractNode* SolarSytemScene::createPlanet(PlanetDef* aPlanetDef){
 }
 //-------------------------------------------------------//
 AbstractNode* SolarSytemScene::createPlanet(RingDef* aRingDef){
+  printf("created ring\n");
   PlanetDef* vPlanetDef = aRingDef->mPlanetDef;
   TransformSeparator* vTSep = new TransformSeparator();
   // ring should be stable as the rotation axis of the planet
