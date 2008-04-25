ae4c1201dbdd074402c7d9fb193e826682efa0b9
dr0iddr0id <dr0iddr0id@fe886383-234b-0410-a1ab-e127868e2f45>
2008-04-25 11:02:34 +0000

    reimplementation of actionbase not using AbstractNode as base anymore

--- OpenGL_SceneGraph_Stussman/Code/Aufgabe4/src/actions/onoffaction.cpp
+++ OpenGL_SceneGraph_Stussman/Code/Aufgabe4/src/actions/onoffaction.cpp
@@ -10,9 +10,9 @@ inline void OnOffAction::apply(){
   toggle();
 }
 //-------------------------------------------------------//
-inline void OnOffAction::accept(AbstractVisitor &aVisitor){
-  aVisitor.visit(*this);
-}
+//inline void OnOffAction::accept(AbstractVisitor &aVisitor){
+//  aVisitor.visit(*this);
+//}
 //-------------------------------------------------------//
 inline void OnOffAction::add(ToggleNode* aToggle){
   ActionBase::add(aToggle);
@@ -20,28 +20,28 @@ inline void OnOffAction::add(ToggleNode* aToggle){
 //-------------------------------------------------------//
 void OnOffAction::on(){
   mOn = true;
-  for(unsigned i=0; i < mChildren.size(); i++){
-    (reinterpret_cast<ToggleNode*>(mChildren[i]))->on();
+  for(unsigned i=0; i < mListeners.size(); i++){
+    (reinterpret_cast<ToggleNode*>(mListeners[i]))->on();
   }
 } 
 //-------------------------------------------------------//
 void OnOffAction::off(){
   mOn = false;
-  for(unsigned i=0; i < mChildren.size(); i++){
-    (reinterpret_cast<ToggleNode*>(mChildren[i]))->off();
+  for(unsigned i=0; i < mListeners.size(); i++){
+    (reinterpret_cast<ToggleNode*>(mListeners[i]))->off();
   }
 } 
 //-------------------------------------------------------//
 void OnOffAction::toggle(){
   if(mOn){
     mOn = false;
-    for(unsigned i=0; i < mChildren.size(); i++){
-      (reinterpret_cast<ToggleNode*>(mChildren[i]))->off();
+    for(unsigned i=0; i < mListeners.size(); i++){
+      (reinterpret_cast<ToggleNode*>(mListeners[i]))->off();
     }
   }else{
     mOn = true;
-    for(unsigned i=0; i < mChildren.size(); i++){
-      (reinterpret_cast<ToggleNode*>(mChildren[i]))->on();
+    for(unsigned i=0; i < mListeners.size(); i++){
+      (reinterpret_cast<ToggleNode*>(mListeners[i]))->on();
     }
   }
 //  for(unsigned i=0; i < mChildren.size(); i++){
