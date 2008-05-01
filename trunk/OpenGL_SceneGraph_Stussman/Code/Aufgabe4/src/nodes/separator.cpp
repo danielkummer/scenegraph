f617591ac6301f676880ccb333bd439caf25ad1e
dr0iddr0id <dr0iddr0id@fe886383-234b-0410-a1ab-e127868e2f45>
2008-05-01 16:16:05 +0000

    blending added, not sure if it really works ring still not lit on both sides

--- trunk/OpenGL_SceneGraph_Stussman/Code/Aufgabe4/src/nodes/separator.cpp
+++ trunk/OpenGL_SceneGraph_Stussman/Code/Aufgabe4/src/nodes/separator.cpp
@@ -15,6 +15,7 @@ void Separator::accept(AbstractVisitor &aVisitor){
   glPushClientAttrib(GL_CLIENT_ALL_ATTRIB_BITS);
   Separator::visitChildren(aVisitor);
   glPopClientAttrib();
+  glColor3f(1, 1, 1);
   aVisitor.postvisit(*this);
 }
 //----------------------------------------------------------//
