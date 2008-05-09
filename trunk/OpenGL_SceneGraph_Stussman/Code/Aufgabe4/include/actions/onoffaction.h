a00b729a7c715b3a5f81ed7e1dced04e83ca4ad0
david.gunzinger@gmail.com <david.gunzinger@gmail.com@fe886383-234b-0410-a1ab-e127868e2f45>
2008-05-09 19:22:01 +0000

    oops

--- trunk/OpenGL_SceneGraph_Stussman/Code/Aufgabe4/include/actions/onoffaction.h
+++ trunk/OpenGL_SceneGraph_Stussman/Code/Aufgabe4/include/actions/onoffaction.h
@@ -10,7 +10,7 @@
 class OnOffAction:public ActionBase{
 public:
   virtual ~OnOffAction();
-  OnOffAction();
+//  OnOffAction();
   virtual void apply();
 //  virtual void accept(AbstractVisitor &aVisitor);
   virtual void add(ToggleNode* aToggle);
