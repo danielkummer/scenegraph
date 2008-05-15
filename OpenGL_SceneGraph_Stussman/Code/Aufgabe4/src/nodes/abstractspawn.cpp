5b5d102acfd47d56fc7eef5fc6143701366ca1c1
david.gunzinger@gmail.com <david.gunzinger@gmail.com@fe886383-234b-0410-a1ab-e127868e2f45>
2008-05-15 21:27:02 +0000

    abstractspawn.cpp

---
+++ OpenGL_SceneGraph_Stussman/Code/Aufgabe4/src/nodes/abstractspawn.cpp
@@ -0,0 +1,14 @@
+#include "nodes/abstractspawn.h"
+
+AbstractSpawn::AbstractSpawn(GroupNode* aGroupNode):AbstractNode(){
+    mGroupNode = aGroupNode;
+    mFire = false;
+}
+
+void AbstractSpawn::spawn(){
+   mFire = true;
+}
+
+void AbstractSpawn::add(AbstractNode* aNode){
+   mGroupNode->add(aNode);
+}
