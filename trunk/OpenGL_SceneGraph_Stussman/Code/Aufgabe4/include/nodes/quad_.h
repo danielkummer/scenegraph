096d9d134c14236ade5f8f10ba50fdfc1b4acd48
dr0iddr0id <dr0iddr0id@fe886383-234b-0410-a1ab-e127868e2f45>
2008-06-02 12:05:54 +0000

    renamed

---
+++ trunk/OpenGL_SceneGraph_Stussman/Code/Aufgabe4/include/nodes/quad_.h
@@ -0,0 +1,59 @@
+#ifndef _H_Quad_
+#define _H_Quad_
+
+#include "nodes/abstractnode.h"
+
+struct SPoint3D{
+  SPoint3D();
+  SPoint3D(float aX, float aY, float aZ);
+  float x, y, z;
+  SPoint3D cross(SPoint3D other);
+  SPoint3D normalize();
+};
+
+class Quad: public AbstractNode{
+public:
+  Quad(float aPosX, float aPosY, float aPosZ,
+       float aOffX=0, float aOffY=0, float aOffZ=0);
+  Quad(float aPosX, float aPosY, float aPosZ,
+       float aNormX, float aNormY, float aNormZ,
+       float aUpX, float aUpY, float aUpZ,
+       float aOffX, float aOffY, float aOffZ);
+  Quad(float aWidth, float aHeight,
+       float aPosX, float aPosY, float aPosZ,
+       float aNormX, float aNormY, float aNormZ,
+       float aUpX, float aUpY, float aUpZ,
+       float aOffX=0, float aOffY=0, float aOffZ=0);
+  Quad(float aP1X, float aP1Y, float aP1Z,
+       float aP2X, float aP2Y, float aP2Z,
+       float aP3X, float aP3Y, float aP3Z,
+       float aP4X, float aP4Y, float aP4Z,
+       float aPosX, float aPosY, float aPosZ,
+       float aOffX=0, float aOffY=0, float aOffZ=0);
+  Quad(float aP1X, float aP1Y, float aP1Z,
+       float aP2X, float aP2Y, float aP2Z,
+       float aP3X, float aP3Y, float aP3Z,
+       float aP4X, float aP4Y, float aP4Z,
+       float aPosX, float aPosY, float aPosZ,
+       float aNormX, float aNormY, float aNormZ,
+       float aUpX, float aUpY, float aUpZ,
+       float aOffX=0, float aOffY=0, float aOffZ=0);
+  virtual ~Quad();
+
+  virtual void accept(AbstractVisitor &aVisitor);
+
+
+  SPoint3D mNormal;
+  SPoint3D mPos;
+  SPoint3D mOffset;
+  SPoint3D mP1;
+  SPoint3D mP2;
+  SPoint3D mP3;
+  SPoint3D mP4;
+
+};
+
+
+#endif // _H_Quad_
+
+
